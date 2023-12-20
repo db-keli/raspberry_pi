0c05873a41da1e06004382c09e236f7333d34fea
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2023-12-20 20:25:40 +0000

    did a bit

--- project_1.py
+++ project_1.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
+
 PIN11 = 11
 PIN13 = 13
 PIN15 = 15
@@ -8,13 +9,27 @@ PIN16 = 16
 PIN18 = 18
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(18, GPIO.OUT)
+GPIO.setup(PIN11, GPIO.OUT)
+GPIO.setup(PIN13, GPIO.OUT)
+GPIO.setup(PIN15, GPIO.OUT)
+GPIO.setup(PIN16, GPIO.OUT)
+GPIO.setup(PIN18, GPIO.OUT)
 
-for i in range(20):
-    print(f"This is iteration {i}")
-    GPIO.output(18, 1)
+while True:
+    GPIO.output(PIN18, 1)
     time.sleep(1)
-    GPIO.output(18, 0)
+    GPIO.output(PIN18, 0)
     time.sleep(1)
+    GPIO.output(PIN16, 1)
+    time.sleep(1)
+    GPIO.output(PIN16, 0)
+    time.sleep(1)
+    GPIO.output(PIN18, 1)
+    GPIO.output(PIN16, 1)
+    time.sleep(3)
+    GPIO.output(PIN18, 0)
+    GPIO.output(PIN16, 0)
+    time.sleep(1)
+    break
 
 GPIO.cleanup()
